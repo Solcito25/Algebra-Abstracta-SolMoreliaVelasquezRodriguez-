76cc49d8c05f9be50e122bce7020124f886f09c7
Solcito25 <solmorelia25@gmail.com>
2020-04-26 21:33:00 -0500

    Euclides

--- Euclides/main.cpp
+++ Euclides/main.cpp
@@ -18,9 +18,8 @@ return b;
 }
 ZZ euclides1(ZZ a,ZZ b){
 ZZ r=mod(a,b);
-     //cout<<"a"<<"    "<<"b"<<endl;
     do{
-        //cout<<a<<"    "<<b<<endl;
+        cout<<" a "<<a<<" b "<<b<<endl;
         a=b;
         b=r;
         r=mod(a,b);
@@ -31,9 +30,8 @@ ZZ r=mod(a,b);
 }
 ZZ euclides2(ZZ a,ZZ b){
     ZZ r=mod(a,b);
-     //cout<<"a"<<"    "<<"b"<<endl;
     do{
-        //cout<<a<<"    "<<b<<endl;
+        cout<<" a "<<a<<" b "<<b<<endl;
         if(r>b/2)
         r=b-r;
         a=b;
@@ -46,37 +44,37 @@ ZZ euclides2(ZZ a,ZZ b){
 ZZ euclides3 (ZZ a, ZZ b){
     if(b==0)
         return a;
-    //cout<<"a"<<a<<"    "<<"b"<<b<<endl;
+    cout<<" a "<<a<<" b "<<b<<endl;
     return euclides3(b,mod(a,b));
 }
 ZZ euclides4(ZZ a, ZZ b){
 ZZ x(2);
-//cout<<"a"<<a<<"    "<<"b"<<b<<endl;
+cout<<" a "<<a<<"    "<<" b "<<b<<endl;
 if(ValAbs(b)>ValAbs(a))return euclides4(b,a);
 else if (b==0) return a;
-else if(mod(a,x)==0&&mod(b,x)==0){return 2*(euclides4(a/2,b/2));}
-else if(mod(a,x)==0&&mod(b,x)!=0){return euclides4(a/2,b);}
-else if(mod(a,x)!=0&&mod(b,x)==0){return euclides4(a,b/2);}
-else {return euclides4((ValAbs(a)- ValAbs(b))/2,b);}
+else if(mod(a,x)==0&&mod(b,x)==0){return 2*(euclides4(a/2,b/2));cout<<" a "<<a<<"    "<<" b "<<b<<endl;}
+else if(mod(a,x)==0&&mod(b,x)!=0){return euclides4(a/2,b);cout<<" a "<<a<<"    "<<" b "<<b<<endl;}
+else if(mod(a,x)!=0&&mod(b,x)==0){return euclides4(a,b/2);cout<<" a "<<a<<"    "<<" b "<<b<<endl;}
+else {return euclides4((ValAbs(a)- ValAbs(b))/2,b);cout<<" a "<<a<<"    "<<" b "<<b<<endl;}
 
 }
 ZZ euclides5(ZZ a, ZZ b){
-ZZ g(1);ZZ x(2);
+ZZ g(1);ZZ x(2);ZZ t;
 while (mod(a,x)==0&&mod(b,x)==0){
     a=a/2;b=b/2;g=2*g;
-    //cout<<" a "<<a<<" b "<<b<<" g "<<g<<endl;
+    cout<<" a "<<a<<" b "<<b<<" g "<<g<<endl;
 }
 while(a!=0){
     while(mod(a,x)==0){a=a/2;
-    //cout<<" a "<<a<<endl;
+    cout<<" a "<<a<<" b "<<b<<" t "<<t<<endl;
     }
     while(mod(b,x)==0){b=b/2;
-    //cout<<" a "<<a<<endl;
+    cout<<" a "<<a<<" b "<<b<<" t "<<t<<endl;
     }
-    ZZ t((ValAbs(a-b))/2);
+    t=((ValAbs(a-b))/2);
     if(a>=b)a=t;
     else b=t;
-    //cout<<" a "<<a<<" b "<<b<<" t "<<t<<endl;
+    cout<<" a "<<a<<" b "<<b<<" t "<<t<<endl;
 }
 //cout<<a<<"    "<<b<<endl;
 return g*b;
@@ -84,13 +82,13 @@ return g*b;
 }
 
 ZZ euclides6(ZZ a, ZZ b){
-    //cout<<"r1"<<"    "<<"r2"<<endl;
+    cout<<"r1"<<"    "<<"r2"<<endl;
     while(a!=b){
         if(a>b)
             a=a-b;
         else
             b=b-a;
-       //cout<<a<<"    "<<b<<"    "<<endl;
+       cout<<" a "<<a<<" b "<<b<<endl;
     }
 
 return a;
@@ -98,7 +96,17 @@ return a;
 ZZ euclides7(ZZ a,ZZ b){
     ZZ r(mod(a,b));
     if(r==0)return b;
+    cout<<" a "<<a<<" b "<<b<<endl;
     return euclides7(b,r);
+}
+ZZ euclides8(ZZ a,ZZ b){
+while(b>0){
+    ZZ q(a/b);
+    ZZ r(a-q*b);
+    a=b;b=r;
+    cout<<" a "<<a<<" b "<<b<<endl;
+}
+return a;
 }
 int main()
 {
@@ -108,18 +116,20 @@ int main()
     cin >> a;
     cin >> b;
     cout << "Algoritmo 1 \n";
-    cout <<euclides1(a,b)<<"\n";
+    cout <<"MCD "<<euclides1(a,b)<<"\n";
     cout << "Algoritmo 2 \n";
-    cout <<euclides2(a,b)<<"\n";
+    cout <<"MCD "<<euclides2(a,b)<<"\n";
     cout << "Algoritmo 3 \n";
-    cout <<euclides3(a,b)<<"\n";
+    cout <<"MCD "<<euclides3(a,b)<<"\n";
     cout << "Algoritmo 4 \n";
-    cout <<euclides4(a,b)<<"\n";
+    cout <<"MCD "<<euclides4(a,b)<<"\n";
     cout << "Algoritmo 5 \n";
-    cout <<euclides5(a,b)<<"\n";
+    cout <<"MCD "<<euclides5(a,b)<<"\n";
     cout << "Algoritmo 6 \n";
-    cout <<euclides6(a,b)<<"\n";
+    cout <<"MCD "<<euclides6(a,b)<<"\n";
     cout << "Algoritmo 7 \n";
-    cout <<euclides7(a,b)<<"\n";
+    cout <<"MCD "<<euclides7(a,b)<<"\n";
+    cout << "Algoritmo 8 \n";
+    cout <<"MCD "<<euclides8(a,b)<<"\n";
     return 0;
 }
